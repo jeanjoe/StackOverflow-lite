5a2d1eb37d60e165ba24bf04e7eb5062bcef3768
jeanjoe <manzede@gmail.com>
2018-08-16 12:40:33 +0300

    [Delivers #159818611] set root directory route

--- api.py
+++ api.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+#Test API Root Directory
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({
+        'message': 'Kudos, API endpoints Work. Follow /api/v1/questions to get Questions', 
+        'success': 1 
+        })
 
-if _name__ == '__main__':
-    app.run(debug==True)
+if __name__ == '__main__':
+    app.run(debug=True)
