f08b29dd07233237e1cea7fb11367fe3f0ff4d04
jeanjoe <manzede@gmail.com>
2018-08-18 15:47:07 +0300

    [Fixes #159849210] rename app folder to api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import unittest, json, requests
 from datetime import datetime
-from app.models import ManageQuestions
+from api.models import ManageQuestions
 
 BASE_URL = 'http://127.0.0.1:5000/api/v1/questions'
 
