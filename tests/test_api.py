19bcb5c4b0addb043c811f509bd3f81228823e15
jeanjoe <manzede@gmail.com>
2018-08-18 13:16:21 +0300

    [Delivers #159847453] add more tests and remove sample data for questions

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,25 +6,6 @@ BASE_URL = 'http://127.0.0.1:5000/api/v1/questions'
 
 question_manager = app.ManageQuestions()
 
-question_data = [
-    {
-        'id': 1000,
-        'author': 5,
-        'title': 'Mocking Test title',
-        'body': 'Mocking Test body Text',
-        'created_at': str(datetime.now()),
-        'tags': 'Test, Python'
-    },
-    {
-        'id': 2000,
-        'author': 5,
-        'title': 'Mocking Test title',
-        'body': 'Mocking Test body Text',
-        'created_at': str(datetime.now()),
-        'tags': 'Test, Python'
-    }
-]
-
 class TestApi(unittest.TestCase):
 
     def setUp(self):
@@ -37,8 +18,8 @@ class TestApi(unittest.TestCase):
     def test_post_question(self):
         test_data = {
             'author': 1,
-            'title': 'Test Title',
-            'body': 'Test text body',
+            'title': 'Does my post question reach the end point?',
+            'body': 'Am testing my Api End point for posting Questions',
             'tags': 'Test, Python'
         }
         response = requests.post(BASE_URL, params= test_data)
@@ -48,12 +29,36 @@ class TestApi(unittest.TestCase):
     def test_get_specific_question(self):
         response = requests.get(BASE_URL+'/1')
         self.assertEqual(1, response.json()['success'])
+    
+    def test_get_specific_question_not_found(self):
+        response = requests.get(BASE_URL+'/1000')
+        expected_response = {
+            'success': 0,
+            'message': 'Unable to find Question with ID 1000'
+        }
+        self.assertEqual(expected_response, response.json())
 
-    def test_delete_question(self):
-        response = requests.delete(BASE_URL + '/3?author=1')
+    def test_successful_delete_question(self):
+        response = requests.delete(BASE_URL + '/2?author=1')
         expected_response = {
             'success': 1,
             'message': 'Question Removed successfully'
+        }
+        self.assertEqual(expected_response, response.json())
+
+    def test_delete_question_not_found(self):
+        response = requests.delete(BASE_URL + '/12?author=1')
+        expected_response = {
+            'success': 0, 
+            'message': 'Unable to find Question with ID 12'
+        }
+        self.assertEqual(expected_response, response.json())
+
+    def test_unauthorised_delete_question(self):
+        response = requests.delete(BASE_URL + '/1?author=5')
+        expected_response = {
+            'success': 0, 
+            'message': 'You donot have permission to delete this Question 1'
         }
         self.assertEqual(expected_response, response.json())
  
