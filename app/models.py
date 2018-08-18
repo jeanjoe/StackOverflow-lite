fe6f8393e375bc90c29618f17dd9d3b3dbd4317f
jeanjoe <manzede@gmail.com>
2018-08-18 11:19:22 +0300

    [Delivers #159868110] restruture files

---
+++ app/models.py
@@ -0,0 +1,31 @@
+
+
+class ManageQuestions():
+    def __init__(self, questions = [], answers = []):
+        self.questions = questions
+        self.answers = answers
+    
+    #Method to search for Question Answers if found, return question and answers else return False
+    def question_answers(self, question_ID):
+        if self.search_question == False:
+            return False
+        try:
+            searched_answers = []
+            if len(self.answers) > 0:
+                for i in self.answers:
+                    if i['question_id'] == question_ID:
+                        searched_answers.append(i)
+                return searched_answers
+            return []
+        except:
+            return []
+
+    #Method to Search for a Question and if found, return the Question else return False
+    def search_question(self, question_ID):
+        if len(self.questions) > 0:
+            try:
+                question = next( question for question in self.questions if question['id'] == question_ID)
+                return question
+            except:
+                return False
+        return False
