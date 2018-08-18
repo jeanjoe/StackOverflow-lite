789549bfd1ce585c31dd0f5f0373f78f712fc98c
jeanjoe <manzede@gmail.com>
2018-08-18 18:33:45 +0300

    [Fixes #159849210] change BASE_URL linkt to point to heroku

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,8 @@ import unittest, json, requests
 from datetime import datetime
 from api.models import ManageQuestions
 
-BASE_URL = 'http://127.0.0.1:5000/api/v1/questions'
+# BASE_URL = 'http://127.0.0.1:5000/api/v1/questions'
+BASE_URL = 'https://manzede-stackoverflow-lite.herokuapp.com/'
 
 question_manager = ManageQuestions()
 
