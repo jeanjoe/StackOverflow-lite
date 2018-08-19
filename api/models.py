23cc284088b58dd2e3a700bd962480f3df4555d5
jeanjoe <manzede@gmail.com>
2018-08-19 08:42:05 +0300

    [Fixes #159871680] remove question length check

--- api/models.py
+++ api/models.py
@@ -28,14 +28,11 @@ class ManageQuestions():
     #Method to search for Question Answers if found, return question and answers else return False
     def question_answers(self, question_ID):
         searched_answers = []
-        # if len(self.answers) > 0:
         for i in self.answers:
             if i['question_id'] == question_ID: searched_answers.append(i)
         return searched_answers
             
     #Method to Search for a Question and if found, return the Question else return False
     def search_question(self, question_ID):
-        if len(self.questions) > 0:
-            question = next((question for question in self.questions if question['id'] == question_ID), None)
-            return question
-        return False
+        question = next((question for question in self.questions if question['id'] == question_ID), None)
+        return question
