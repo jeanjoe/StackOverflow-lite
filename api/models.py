9f353ed242274bc8efba132e680150bacf14d63d
jeanjoe <manzede@gmail.com>
2018-08-19 08:30:54 +0300

    [Fixes #159871680] reduce if statements to reduce complexity

--- api/models.py
+++ api/models.py
@@ -27,13 +27,11 @@ class ManageQuestions():
     
     #Method to search for Question Answers if found, return question and answers else return False
     def question_answers(self, question_ID):
-        if self.search_question == False or self.search_question == None: return False
-            
         searched_answers = []
-        if len(self.answers) > 0:
-            for i in self.answers:
-                if i['question_id'] == question_ID:
-                    searched_answers.append(i)
+        if self.search_question != False or not self.search_question != None:
+            if len(self.answers) > 0:
+                for i in self.answers:
+                    if i['question_id'] == question_ID: searched_answers.append(i)
         return searched_answers
             
     #Method to Search for a Question and if found, return the Question else return False
