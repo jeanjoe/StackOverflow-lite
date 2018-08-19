233efe4d47c5acc9a7cd8fc3b55b16a6ebca19e8
jeanjoe <manzede@gmail.com>
2018-08-19 08:38:24 +0300

    [Fixes #159871680] remove answer length check

--- api/models.py
+++ api/models.py
@@ -28,9 +28,9 @@ class ManageQuestions():
     #Method to search for Question Answers if found, return question and answers else return False
     def question_answers(self, question_ID):
         searched_answers = []
-        if len(self.answers) > 0:
-            for i in self.answers:
-                if i['question_id'] == question_ID: searched_answers.append(i)
+        # if len(self.answers) > 0:
+        for i in self.answers:
+            if i['question_id'] == question_ID: searched_answers.append(i)
         return searched_answers
             
     #Method to Search for a Question and if found, return the Question else return False
